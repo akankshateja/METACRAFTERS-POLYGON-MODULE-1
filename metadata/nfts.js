93df3cfb4a2b7171ce20fe5578cf8859a25bbb4c
Palakkkkkkkkkk <133787068+Palakkkkkkkkkk@users.noreply.github.com>
2023-07-25 16:22:46 +0530

    nfts.js

---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'a cute girl in blue dress black hair wayfarer',
+        image: 'QmRaLELJNNAGttkwU78z3SW3Aq2vNH1oAXZMomntFWSPAa'
+    },
+    {
+        name: '1',
+        description: 'a cute girl in blue dress black hair wayfarer',
+        image: 'QmbTPMdFyShtejrnSYs7XeTV8kAzcC6jjjdMjFQBpgKZf1'
+    },//change
+    {
+        name: '2',
+        description: 'a cute girl in blue dress black hair wayfarer',
+        image: 'Qmd33LKQZqcPuwYQ5AWN8beo5PuTC1cJV4dofXAhevcjwZ'
+    },
+    {
+        name: '3',
+        description: 'a cute girl in blue dress black hair wayfarer',
+        image: 'QmRcRbuxFscuRB1TAZLrK1HkS4uQwDuoUDnrwUEaRVovw5'
+    },
+    {
+        name: '4',
+        description: 'This is the ultimate image NFT in the Indian NFT collection.',
+        image: 'QmRTNiwxEvLi3eUqTq9m3kr4As2ffKVHcG8VaWMyj2wzXb'
+    },    
+];
+
+module.exports = nfts;
